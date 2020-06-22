8777535820bfad66fee13f9b5958c7a5d050fcfb
Nevhuhulwip <pfanonevhuhulwi@gmail.com>
2020-06-22 09:48:55 +0200

    second commit

--- src/main/java/MainProgram.java
+++ src/main/java/MainProgram.java
@@ -1,28 +1,31 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Scanner;
 
 public class MainProgram {
+
+    public static final Logger logger = LogManager.getLogger(PasswordChecker.class.getName());
+
     public static void main(String[] args) {
         PasswordChecker password = new PasswordChecker();
-
         Scanner in = new Scanner(System.in);
         {
             System.out.println("Please enter password : ");
             String yourPassword = in.nextLine();
-            System.out.println();
+
             try{
                 if(password.isValid(yourPassword )){
-                    System.out.println("Password is valid");
+                    System.out.println("User password ok");
                 }
                 if(password.passwordIsOk()){
-                    System.out.println("Password is okay");
+                    logger.debug("Password is okay");
                 }
-                if(password.PasswordNeverOk(yourPassword)){
-                }
-            }catch(Exception e){
-                System.out.println(e.getMessage());
+
+            }
+            catch(Exception e){
+                logger.error(e.getMessage());
             }
         }
     }
-
 }
--- src/main/java/PasswordChecker.java
+++ src/main/java/PasswordChecker.java
@@ -1,10 +1,13 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.regex.Pattern;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
+import java.util.regex.Pattern;
 public class PasswordChecker {
-    private static ArrayList<String> listOfErrors= new ArrayList<>();
-    public static boolean isValid(String yourPassword ){
+    static final Logger logger = LogManager.getLogger(PasswordChecker.class.getName());
+    static ArrayList<String> listOfErrors = new ArrayList<>();
+
+    public static boolean isValid(String yourPassword) {
         Pattern specialCharPatten = Pattern.compile("[$&+,:;=?@#|'<>.^*()%!-]", Pattern.CASE_INSENSITIVE);
         Pattern digitCasePatten = Pattern.compile("[0-9]");
         Pattern UpperCasePatten = Pattern.compile("[A-Z]");
@@ -12,54 +15,54 @@ public class PasswordChecker {
 
         boolean flag = true;
 
-        if(yourPassword.isEmpty()){
-            System.out.println("password does not exist ");
+        if (yourPassword.isEmpty()) {
+            logger.error("password does not exist ");
             flag = false;
-            listOfErrors.add("password does not exist ");
+
         }
         if (yourPassword.length() < 8) {
-            System.out.println("Password length must have at least 8 character ");
+            logger.error("Password length must have at least 8 character ");
             flag = false;
-            listOfErrors.add("Password length must have at least 8 character ");
+
         }
-        if (!specialCharPatten.matcher(yourPassword ).find()) {
-            System.out.println("Password must have at least one special character ");
+        if (!specialCharPatten.matcher(yourPassword).find()) {
+            logger.error("Password must have at least one special character ");
             flag = false;
-            listOfErrors.add("Password must have at least one special character ");
+
         }
-        if (!UpperCasePatten.matcher(yourPassword ).find()) {
-            System.out.println("Password must have at least one uppercase character ");
+        if (!UpperCasePatten.matcher(yourPassword).find()) {
+            logger.error("Password must have at least one uppercase character ");
             flag = false;
-            listOfErrors.add("Password must have at least one uppercase character ");
+
         }
-        if (!lowerCasePatten.matcher(yourPassword ).find()) {
-            System.out.println("Password must have at least one lowercase character ");
+        if (!lowerCasePatten.matcher(yourPassword).find()) {
+            logger.error("Password must have at least one lowercase character ");
             flag = false;
-            listOfErrors.add("Password must have at least one lowercase character ");
+
         }
-        if (!digitCasePatten.matcher(yourPassword ).find()) {
-            System.out.println("Password must have atleast one digit character ");
+        if (!digitCasePatten.matcher(yourPassword).find()) {
+            logger.error("Password must have at least one digit character ");
             flag = false;
-            listOfErrors.add("Password must have atleast one digit character ");
+
         }
         return flag;
     }
-    public static boolean passwordIsOk(){
+
+    public static boolean passwordIsOk() {
         boolean flag = false;
-        if(listOfErrors.size()<=3){
+        if (listOfErrors.size() <= 3) {
             flag = true;
         }
         return flag;
     }
-    public static boolean PasswordNeverOk(String password){
+
+    public static boolean PasswordNeverOk(String password) {
         boolean flag = false;
-        if(password.isEmpty() && password.length() < 8) {
+        if (password.isEmpty() && password.length() < 8) {
             return flag;
-        }else{
-            System.out.println("Password meets condition 1 and 2");
-        }
-        return flag;
+
+       }
+            return flag;
+
     }
-
-
 }
